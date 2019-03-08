f5d33a0ac8fa58fb81346942d20dd77fcc1e1b6f
Harrison J Goldwyn <goldwyn@uw.edu>
2019-03-07 17:18:06 -0800

    Ellaborated contents ofhigh level class which takes paper as input and summzarizes content.

--- paperparser/extracted_info.py
+++ paperparser/extracted_info.py
@@ -41,7 +41,7 @@ In dictionary form (following implementaion):
 
     object = {
         'material identifier' : 'ABCD',
-        {
+            'properties'v : {
             'Performance metrics' : {
                 'PCE' : {
                     'value' : (number),
@@ -86,12 +86,94 @@ Development Notes:
 """
 from parse import parse_pce, parse_synthesis
 
+
+class ExtractSynthesisPerformance(object):
+    """ Performs data extraction from paper
+        """
+
+    def __init__(self, paper):
+        """ Run components to extract information from paper """
+
+        # convert paper to text...
+
+        # Flag paragraphs, will be formated as;
+        # {
+        #     'performance' : [ list of paragraph strings ],
+        #     'synthesis' : [ list of paragraph strings ],
+        #     }
+        flagged_paragraphs = neels_function(paper)
+
+
+        # Christine takes synthesis sentence and extracts parameters
+        (anneal_parameters, spincoat_parameters) = christines_function(
+            flagged_paragraphs['synthesis']
+            )
+
+        # Order steps in synthesis
+        ordered_step_list = linnettes_function(synthesis_paragraph)
+
+        # Parse paper for PCE
+        pce_info = parse_pce(flagged_paragraphs['performance'])
+
+        # ECT for other performance metrics
+
+        wrapped_info_object = build_realtional_dict
+        # ....
+
+    return dictionary_of_relational_results
+
+
+def neels_function(plain_txt):
+    """ Probably will be imported from another file
+
+    for paragraph in plain_txt:
+        for sentence in paragraph:
+            if sentence about synthesis:
+                this paragraph should be flagged as synthesis
+            else: pass
+
+            if this sentence talk about performance metric?
+                this paragraph flagged as performance
+        """
+    pass
+
+
+def christines_function(sentences_or_paragraphs_specific_to_synthesis):
+    """ Probably will be imported from another file """
+    pass
+
+def linnettes_function(sentences_or_paragraphs_specific_to_synthesis):
+    """ Probably will be imported from another file """
+    pass
+
+
+class PerformanceMetric(object):
+    """
+        """
+
+    def __init__(self,
+        name,
+        value,
+        units):
+        """
+            Args:
+
+                name: string containing name such as; 'PCE', 'VOC', 'JSC', ect
+
+                value: number
+
+                units: string such as; percent, '%', 'V', 'A'
+            """
+        self.name = name
+        self.value = value
+        self.units = units
+
+
 class ActiveMaterialGraph(PerformanceMetrics, SynthesisParameters):
     """ NOTE: Not sure that this is worth doing as implemented. What
             we probably want is a way to build the component
             dictionarie. I we should talk about this.
 
-        Update
 
         Active material extracted from paper and its properties. This
         class serves as a wrapper for the dictionary structure
@@ -139,8 +221,6 @@ class ActiveMaterialGraph(PerformanceMetrics, SynthesisParameters):
                     and relevant parameters.
             """
 
-        self.performace_metrics = performace_metrics
-        self.synthesis_steps = synthesis_steps
 
         # unsure how to do this right now, but what I want is an easy way to build the
         # Unfinished outline ...
