62fab984f21154dd21d446ef0c9934771f9cfc6d
Christine Chang <changch@uw.edu>
2019-03-14 14:32:19 -0700

    minor changes

--- paperparser/parse/__init__.py
+++ paperparser/parse/__init__.py
@@ -1,18 +1,28 @@
 """
 paperparser.parse
-~~~~~~~~~~~~~~~~~~
+~~~~~~~~~~~~~~~~~
 
-Module containing tootls to parse synthesis parameters and pce from
-perovskite paper text.
+Module containing tootls to parse synthesis parameters and PCE (power
+conversion efficiency) from perovskite paper text.
 
 Built on top of ChemDataExtractor version 1.3.0, copyright 2017 Matt
 Swain and contributors.
-github repo: ...
+github repo: .../mcs07/ChemDataExtractor/
 
 """
+# External Imports
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+from __future__ import unicode_literals
 
 from chemdataextractor import parse
 
-from .pce import PceParser
+ # Function Imports
+ from .spincoat import parse_spincoat
+ from .anneal import parse_anneal
+
+# Parser imports
 from .spincoat import SpinCoatParser
 from .anneal import AnnealParser
+from .pce import PceParser
