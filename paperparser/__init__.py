4af76d66541a50f02350b8aada54bd99db3050fa
Christine Chang <changch@uw.edu>
2019-03-14 12:16:05 -0700

    init file, modeled after CDE, and other minor updates

---
+++ paperparser/__init__.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+"""
+PaperParser
+~~~~~~~~~~~
+"""
+
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+from __future__ import unicode_literals
+import logging
+
+
+__title__ = 'PaperParser'
+__version__ = '1.0.0'
+__author__ = 'Christine Chang, Harrison Goldwyn, Linnette Teo, and Neel Shah'
+__license__ = 'MIT'
+__copyright__ = 'Copyright 2019 PaperParser team'
+__status__ = 'Not ready'
+
+log = logging.getLogger(__name__)
+log.addHandler(logging.NullHandler())
