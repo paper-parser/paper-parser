3c6c8ce207c653d5b0523b516a462ffae1c0b6e8
Christine Chang <changch@uw.edu>
2019-03-21 10:55:59 -0700

    update test_spincoat.py

--- paperparser/test/test_spincoat.py
+++ paperparser/test/test_spincoat.py
@@ -3,7 +3,7 @@ from ..parse import spincoat # not sure how to write this correctly
 from chemdataextractor.doc import Sentence
 
 # Test variables and outputs
-test_s = Sentence(u'The resulting solution was coated onto the mp-TiO2/bl-TiO2/FTO substrate by a consecutive two-step spin-coating process at 1,000 and 5,000 r.p.m for 10 and 20 s, respectively.')
+test_s = 'The resulting solution was coated onto the mp-TiO2/bl-TiO2/FTO substrate by a consecutive two-step spin-coating process at 1,000 and 5,000 r.p.m for 10 and 20 s, respectively.'
 test_s_output = spincoat.parse_spincoat(test_s)
 
 # Test functions
@@ -12,13 +12,12 @@ def test_parse_spincoat():
     Test function for spincoat.parse_spincoat
     """
     assert isinstance(test_s, str), 'Incorrect input type: string required'
-    assert isinstance(test_s_output, list), (
+    assert isinstance(test_s_output, list), \
         'Error: incorrect output type (list expected)'
-        )
     assert test_s_output, 'Error: no parameters detected (parsing error)'
     try:
         assert isinstance(test_s_output[0], dict), \
-        'Error: output must be list of dictionaries')
+        'Error: output must be list of dictionaries'
     except IndexError:
         raise Exception('Error: output is not a list, or is an empty list')
     except TypeError:
