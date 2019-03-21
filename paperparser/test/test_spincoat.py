1a9534e39a502019c35d80e5a20e38638a076751
Christine Chang <changch@uw.edu>
2019-03-21 10:33:50 -0700

    updated test_spincoat.py

--- paperparser/test/test_spincoat.py
+++ paperparser/test/test_spincoat.py
@@ -1,5 +1,6 @@
 # Import(s)
 from ..parse import spincoat # not sure how to write this correctly
+from chemdataextractor.doc import Sentence
 
 # Test variables and outputs
 test_s = Sentence(u'The resulting solution was coated onto the mp-TiO2/bl-TiO2/FTO substrate by a consecutive two-step spin-coating process at 1,000 and 5,000 r.p.m for 10 and 20 s, respectively.')
@@ -17,3 +18,7 @@ def test_parse_spincoat():
     try:
         assert isinstance(test_s_output[0], dict), 'Error: output must be \
                                                     list of dictionaries'
+    except IndexError:
+        raise Exception('Error: output is not a list, or is an empty list')
+    except TypeError:
+        raise Exception('Error: output must be a list of dictionaries')
