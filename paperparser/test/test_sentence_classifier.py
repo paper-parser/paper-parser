c3b2ebcc29c4e42a2742807cd22e08739063fbc1
Christine Chang <changch@uw.edu>
2019-03-21 23:03:22 -0700

    trying something diff with pkl

--- paperparser/test/test_sentence_classifier.py
+++ paperparser/test/test_sentence_classifier.py
@@ -1,12 +1,13 @@
 import os
-
+import requests
 from paperparser.read_paper import sentence_classifier
 import sklearn
 from sklearn.pipeline import Pipeline
 from sklearn.externals import joblib
 import numpy
 
-with open(os.path.join(os.path.dirname(__file__), 'syn_sen_model.pkl'), 'rb') as syn_pickle:
+req = requests.get('https://github.com/paper-parser/paper-parser/blob/master/paperparser/syn_sen_model.pkl?raw=true')
+with open('syn_sen_model.pkl', 'rb') as syn_pickle:
     syn_sen_model = joblib.load(syn_pickle) #imported pre-trained model for classfying sentence
 
 def test_spacy_tokenizer():
